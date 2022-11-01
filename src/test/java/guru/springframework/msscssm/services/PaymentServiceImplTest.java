b8e40fa0691c28bd4640b9725ad5544d094e176d
Kaiotech <kaiotech@icloud.com>
2022-11-01 15:16:34 +0100

    pre auth to auth

--- src/test/java/guru/springframework/msscssm/services/PaymentServiceImplTest.java
+++ src/test/java/guru/springframework/msscssm/services/PaymentServiceImplTest.java
@@ -44,4 +44,21 @@ class PaymentServiceImplTest {
 
         System.out.println(preAuthedPayment);
     }
+
+    @Test
+    void testAuth() {
+        Payment savedPayment = paymentService.newPayment(payment);
+
+        StateMachine<PaymentState, PaymentEvent> preAuthSM = paymentService.preAuth(savedPayment.getId());
+
+        if (preAuthSM.getState().getId() == PaymentState.PRE_AUTH) {
+            System.out.println("Payment is Pre Authorized");
+
+            StateMachine<PaymentState, PaymentEvent> authSM = paymentService.authorizePayment(savedPayment.getId());
+
+            System.out.println("Result of Auth: " + authSM.getState().getId());
+        } else {
+            System.out.println("Payment failed pre-auth...");
+        }
+    }
 }
